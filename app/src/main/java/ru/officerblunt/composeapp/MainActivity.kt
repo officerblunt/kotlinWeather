bf567711d126919d632d0ae83e93c9587a34655f
student <student@fspo.itmo.ru>
2021-11-13 14:58:07 +0300

    onclick, containers

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -4,17 +4,16 @@ package ru.officerblunt.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -22,17 +21,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val myColor: Color = Color(0xFFFFC107);
-            Text(
-                "What is Lorem Ipsum?\n",
-                fontSize = 22.sp,
-                modifier = Modifier
-                    .padding(5.dp)
-                    .background(myColor)
-                    .fillMaxSize()
-                    .horizontalScroll(ScrollState(0))
-                    .verticalScroll(ScrollState(0))
-            )
+            val count = remember { mutableStateOf(0) }
+            Column()
+            {
+                Box(modifier = Modifier.background(Color.Red).fillMaxWidth().weight(2f))
+                Box(modifier = Modifier.background(Color.Yellow).fillMaxWidth().weight(3f))
+                Box(modifier = Modifier.background(Color.Green).fillMaxWidth().weight(2f))
+                Text(
+                    "Clicks: ${count.value}",
+                    fontSize = 28.sp,
+                    modifier = Modifier
+                        .clickable(onClick = { count.value += 1 })
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+
         }
     }
 
