b790c460b2da2a5ca4970c3f4c74f00eadf24061
officerblunt <cuppetunbluze@gmail.com>
2021-11-19 02:16:05 +0300

    onclick

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -4,16 +4,12 @@ package ru.officerblunt.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -21,21 +17,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember { mutableStateOf(0) }
-            Column()
-            {
-                Box(modifier = Modifier.background(Color.Red).fillMaxWidth().weight(2f))
-                Box(modifier = Modifier.background(Color.Yellow).fillMaxWidth().weight(3f))
-                Box(modifier = Modifier.background(Color.Green).fillMaxWidth().weight(2f))
-                Text(
-                    "Clicks: ${count.value}",
-                    fontSize = 28.sp,
-                    modifier = Modifier
-                        .clickable(onClick = { count.value += 1 })
-                        .align(Alignment.CenterHorizontally)
-                )
-            }
-
+            val msg = mutableStateOf("test")
+            Text (
+                msg.value,
+                modifier = Modifier.clickable(onClick = { msg.value = "test2" })
+            )
         }
     }
 
