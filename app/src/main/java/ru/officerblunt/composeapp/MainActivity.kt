369276b4a44c20357844f41a5d24079aec2d1634
officerblunt <cuppetunbluze@gmail.com>
2021-11-19 16:58:58 +0300

    remember

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -5,22 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val msg = mutableStateOf("test")
-            Text (
-                msg.value,
-                modifier = Modifier.clickable(onClick = { msg.value = "test2" })
+            val (value, setValue) = remember {
+                mutableStateOf("click me!")
+            }
+            Text(
+                text = value,
+                fontSize = 50.sp,
+                color = Color.White,
+                modifier = Modifier.clickable(onClick = {setValue("fuck you!")})
             )
         }
     }
