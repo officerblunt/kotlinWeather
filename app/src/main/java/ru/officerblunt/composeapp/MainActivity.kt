f8afb76ab3683faf2531ad73c82601abe408fc33
student <student@fspo.itmo.ru>
2021-11-20 14:55:21 +0300

    start

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -5,27 +5,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val (value, setValue) = remember {
-                mutableStateOf("click me!")
+            Column() {
+                Box() {
+                    Button(onClick = { /*TODO*/ }) {
+                        Icon(
+                            Icons.Filled.Menu,
+                            contentDescription = "Menu",
+                            modifier = Modifier.size(30.dp),
+                        )
+                    }
+                    Text(text = "Заглушка 1", textAlign = TextAlign.Center)
+                    Button(onClick = { /*TODO*/ }) {
+                        Icon(
+                            Icons.Filled.Add,
+                            contentDescription = "add",
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
             }
-            Text(
-                text = value,
-                fontSize = 50.sp,
-                color = Color.White,
-                modifier = Modifier.clickable(onClick = {setValue("fuck you!")})
-            )
         }
     }
-
 }
+
