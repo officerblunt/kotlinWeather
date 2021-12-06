56fdca003147ca5e2f32bcbff193b0386127a1d7
student <student@fspo.itmo.ru>
2021-12-06 15:05:41 +0300

    блять

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -18,6 +19,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
@@ -28,67 +30,68 @@ class MainActivity : ComponentActivity() {
         setContent {
             val scaffoldState = rememberScaffoldState()
             val scope = rememberCoroutineScope()
-            Scaffold(scaffoldState = scaffoldState,
-                drawerContent={
-                    Text("Пункт меню 1", fontSize = 28.sp)
-                    Text("Пункт меню 2", fontSize = 28.sp)
-                    Text("Пункт меню 3", fontSize = 28.sp)
-                }) {
+            val items = listOf("Главная", "Контакты", "О приложении")
+            Box() {
 
-                Column(
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Image(
-                        bitmap = ImageBitmap.imageResource(R.drawable.back),
-                        contentDescription = "background",
-                        contentScale = ContentScale.FillHeight,
-                    )
+                Image(
+                    bitmap = ImageBitmap.imageResource(R.drawable.back),
+                    contentDescription = "background",
+                    contentScale = ContentScale.FillHeight,
+                )
+                Scaffold(scaffoldState = scaffoldState,
+                    drawerBackgroundColor = Color(0xFFE1F5FE),
+                    drawerContentColor = Color(0xFF0277BD),
+                    drawerScrimColor = Color(0x99E0F7FA),
+                    drawerContent = {
+                        items.forEach { item -> Text(item, fontSize = 28.sp) }
+
+                    }) {
                     Column(
-                        verticalArrangement = Arrangement.Top,
-                        modifier = Modifier.fillMaxWidth()
+                        verticalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            IconButton(modifier = Modifier
-                                .padding(start = 25.dp, top = 40.dp)
-                                .size(30.dp), onClick = {
-                                scope.launch {
-                                    scaffoldState.drawerState.open()
-                                }
-                            }) {
-                                Icon(Icons.Rounded.Menu, contentDescription = "")
-                            }
-                            Text(
-                                text = "10^c",
-                                textAlign = TextAlign.Center,
-                                fontSize = 80.sp,
-                                color = Color.White,
-                                modifier = Modifier.padding(vertical = 40.dp)
-                            )
-                            Button(
-                                modifier = Modifier
-                                    .padding(end = 25.dp, top = 40.dp)
-                                    .size(30.dp),
-                                onClick = { /*TODO*/ }
-                            ) {
-                                Icon(
-                                    Icons.Filled.Add,
-                                    contentDescription = "add",
-                                    modifier = Modifier
-                                        .size(30.dp)
-                                )
-                            }
-                        }
-                        Box(
-                            contentAlignment = Alignment.Center,
+                        Column(
+                            verticalArrangement = Arrangement.Top,
                             modifier = Modifier.fillMaxWidth()
                         ) {
-                            Text(
-                                text = "today's date",
-                                color = Color.White
-                            )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                IconButton(modifier = Modifier
+                                    .padding(start = 25.dp, top = 40.dp)
+                                    .size(30.dp), onClick = {/*TODO*/ }) {
+                                    Icon(Icons.Rounded.Menu, contentDescription = "")
+                                }
+                                Text(
+                                    text = "10^c",
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 80.sp,
+                                    color = Color.White,
+                                    modifier = Modifier.padding(vertical = 40.dp)
+                                )
+                                Button(
+                                    modifier = Modifier
+                                        .padding(end = 25.dp, top = 40.dp)
+                                        .size(30.dp),
+                                    onClick = { /*TODO*/ }
+                                ) {
+                                    Icon(
+                                        Icons.Rounded.Add,
+                                        contentDescription = "add",
+                                        modifier = Modifier
+                                            .size(30.dp)
+                                    )
+                                }
+                            }
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = "today's date",
+                                    color = Color.White
+                                )
+                            }
                         }
                     }
                 }
