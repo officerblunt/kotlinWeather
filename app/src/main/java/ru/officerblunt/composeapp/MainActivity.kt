515e13af1e33d45b9abb9275ce6a935aeee47315
StalkerZero <iron-3@mail.ru>
2021-12-11 14:51:42 +0300

    я сделал хоть что-то полезное

--- app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
+++ app/src/main/java/ru/officerblunt/composeapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -17,6 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -30,61 +33,107 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            @Composable
-            fun Body() {
-                Box {
-                    Column(
-                        verticalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Column(
-                            verticalArrangement = Arrangement.Top,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-                                IconButton(modifier = Modifier
-                                    .padding(start = 25.dp, top = 40.dp)
-                                    .size(30.dp),
-                                    onClick = { /*TODO*/ }) {
-                                    Icon(Icons.Rounded.Menu, contentDescription = "")
-                                }
-                                Text(
-                                    text = "10^c",
-                                    textAlign = TextAlign.Center,
-                                    fontSize = 80.sp,
-                                    color = Color.White,
-                                    modifier = Modifier.padding(vertical = 40.dp)
-                                )
-                                IconButton(
-                                    modifier = Modifier
-                                        .padding(end = 25.dp, top = 40.dp)
-                                        .size(30.dp),
-                                    onClick = { /*TODO*/ }
-                                ) {
-                                    Icon(
-                                        Icons.Rounded.Add,
-                                        contentDescription = "add",
-                                        modifier = Modifier
-                                            .size(30.dp)
-                                    )
-                                }
-                            }
-                            Box(
-                                contentAlignment = Alignment.Center,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                Text(
-                                    text = "today's date",
-                                    color = Color.White
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-
+            //Image(bitmap = ImageBitmap.imageResource(R.drawable.back), contentDescription = "background") не работает /*TODO*/
+            MainScreen()
         }
     }
+}
+
+@Composable
+fun Body() {
+    Box {
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
+                    Text(
+                        text = "10^c",
+                        textAlign = TextAlign.Center,
+                        fontSize = 80.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 40.dp)
+                    )
+
+                }
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "today's date",
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TopBar(onMenuClicked: () -> Unit) {
+    TopAppBar(
+        modifier = Modifier.height(100.dp)
+            .background(Color.Transparent),
+        Color(0),
+        elevation = 0.dp
+
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .padding(start = 25.dp, top = 40.dp)
+                    .size(30.dp),
+                onClick = onMenuClicked
+            ) {
+                Icon(Icons.Rounded.Menu, contentDescription = "menu")
+            }
+            IconButton(
+                modifier = Modifier
+                    .padding(end = 25.dp, top = 40.dp)
+                    .size(30.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Rounded.Add, contentDescription = "add")
+            }
+        }
+    }
+}
+
+@Composable
+fun Drawer() {
+    Column(
+        Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+        repeat(5) { item ->
+            Text(text = "Item number $item", modifier = Modifier.padding(8.dp), color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopBar(onMenuClicked = { coroutineScope.launch { scaffoldState.drawerState.open() } })
+        },
+        content = { Body() },
+        drawerContent = { Drawer() }
+    )
 }
